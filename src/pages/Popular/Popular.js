fe5cb2ba3cea1ca28f2d8bfcee4510709c572eed
Nataliia Kudaieva <natkudaieva@gmail.com>
2023-10-23 16:17:07 +0300

    delete unecessary imports

--- src/pages/Popular/Popular.js
+++ src/pages/Popular/Popular.js
@@ -1,9 +1,5 @@
-import { useState } from "react";
 import RepoItem from "./RepoItem";
 import Tab from "./Tab";
-import Error from "../../components/Error";
-import { useGitHubProfile } from "../../contexts/GitHubContext";
-import Loader from "./Loader";
 
 const Popular = () => {
   const languages = ["All", "JavaScript", "Ruby", "Java", "CSS", "Python"];
@@ -12,8 +8,6 @@ const Popular = () => {
     <div>
       <Tab languages={languages} />
       <RepoItem />
-      {/* {isLoading && <Loader />}
-      {error && <Error error={error} />} */}
     </div>
   );
 };
--- src/pages/Popular/RepoItem.js
+++ src/pages/Popular/RepoItem.js
@@ -1,11 +1,12 @@
 import { useGitHubProfile } from "../../contexts/GitHubContext";
+import Loader from "./Loader";
 
 const RepoItem = () => {
-  const { popularRepos } = useGitHubProfile();
-
+  const { popularRepos, isLoading } = useGitHubProfile();
+  if (isLoading) return <Loader />;
   return (
     <ul className="popular-list">
-      {/* {popularRepos.map((repo, index) => {
+      {popularRepos.map((repo, index) => {
         return (
           <li key={repo.id} className="popular-item">
             <div className="popular-rank">#{index + 1}</div>
@@ -31,7 +32,7 @@ const RepoItem = () => {
             </ul>
           </li>
         );
-      })} */}
+      })}
     </ul>
   );
 };
