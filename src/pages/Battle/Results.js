1e5bbd044d3e9000fc137c3a466ce824e784fcf6
Nataliia Kudaieva <natkudaieva@gmail.com>
2023-10-24 22:04:04 +0300

    count results

--- src/pages/Battle/Results.js
+++ src/pages/Battle/Results.js
@@ -1,59 +1,41 @@
 import { useEffect, useState } from "react";
-import { Link, useLocation } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import PlayerPreview from "./PlayerPreview";
 import WinnerDetails from "./WinnerDetails";
 import Trophy from "../../icons/trophy.png";
 import Confetti from "react-confetti";
-import Error from "../../components/Error";
 import { useGitHubProfile } from "../../contexts/GitHubContext";
 
 const Results = () => {
-  const [winner, setWinner] = useState(null);
   const [showConfetti, setShowConfetti] = useState(false);
-  const location = useLocation();
 
-  const { previewPlayerOne, previewPlayerTwo } = location.state;
-  const [error, setError] = useState("");
+  const { fullProfilePlayerOne, fullProfilePlayerTwo } = useGitHubProfile();
+  const [winner, setWinner] = useState([]);
 
-  const { getFullProfileData, fullProfilePlayerOne, fullProfilePlayerTwo } =
-    useGitHubProfile();
-
-  console.log(fullProfilePlayerOne, fullProfilePlayerTwo, "ashaj");
-
-  // useEffect(() => {
-  //   if (!winner) setShowConfetti(false);
-
-  //   showConfetti ||
-  //     setTimeout(() => {
-  //       setShowConfetti(true);
-  //     }, 4000);
-  // }, [showConfetti, winner]);
-
-  // const countScore = (player) => {
-  //   let score = player?.followers + player?.stars;
-  //   return score;
-  // };
-
-  // useEffect(() => {
-  //   if (previewPlayerOne.id === previewPlayerTwo.id) {
-  //     setError(`Sorry, we can't compare identical profiles. Try another ones`);
-  //     setWinner("");
-  //     setShowConfetti(!showConfetti);
-  //   } else
-  //     countScore(previewPlayerOne) > countScore(previewPlayerTwo)
-  //       ? setWinner(previewPlayerOne)
-  //       : setWinner(previewPlayerTwo);
-  // }, [previewPlayerOne, previewPlayerTwo, showConfetti]);
+  const countScores = (player) => {
+    let score = player?.stars + player?.followers;
+    return score;
+  };
 
   useEffect(() => {
-    const params = new URLSearchParams(location.search);
-    params.get("PlayerOne");
-  });
+    countScores(fullProfilePlayerOne) > countScores(fullProfilePlayerTwo)
+      ? setWinner(fullProfilePlayerOne)
+      : setWinner(fullProfilePlayerTwo);
+  }, [fullProfilePlayerOne, fullProfilePlayerTwo]);
+
+  useEffect(() => {
+    if (!winner) setShowConfetti(false);
+
+    showConfetti ||
+      setTimeout(() => {
+        setShowConfetti(true);
+      }, 4000);
+  }, [showConfetti, winner]);
 
   return (
     <>
       <div className="winner__details">
-        {/* <img src={Trophy} alt="trophy" className="winner__trophy wobble" />
+        <img src={Trophy} alt="trophy" className="winner__trophy wobble" />
         <h1 className="winner__heading">Winner is: </h1>
         <span className="winner">{winner.login}</span>
         <PlayerPreview
@@ -61,7 +43,7 @@ const Results = () => {
           username={winner.name}
           render={<WinnerDetails winner={winner} />}
         />
-        <Link to="/battle">Try again</Link> */}
+        <Link to="/battle">Try again</Link>
       </div>
 
       {!showConfetti && <Confetti />}
